4e73d20fec473d9ffa1b4da7321ef02b60f85e7e
AJ <almatjuvashev@MacBook-Pro-Almat.local>
2020-12-19 23:28:54 +0600

    Completed module

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+
+        }
+
+    }
+}
